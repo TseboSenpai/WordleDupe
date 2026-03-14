13c34fbe908659b1b0f4879fda2e1203f5758ff4
TseboSenpai <tsebom.dev@gmail.com>
2026-03-14 21:27:27 +0200

    fix: resolve TypeScript prop type error in Qwerty component

--- components/Qwerty.tsx
+++ components/Qwerty.tsx
@@ -1,11 +1,24 @@
 import { observer } from 'mobx-react-lite'
 
-export default observer(function Querty({ store }) {
+// 1. Define the shape of your Store
+interface WordleStore {
+  exactGuesses: string[];
+  inexactGuesses: string[];
+  allGuesses: string[];
+}
+
+// 2. Define the props for this component
+interface QuertyProps {
+  store: WordleStore;
+}
+
+// 3. Tell the function to use those props
+export default observer(function Querty({ store }: QuertyProps) {
   const qwerty = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm']
   return (
     <div>
-      {qwerty.map((row) => (
-        <div className="flex justify-center">
+      {qwerty.map((row, i) => (
+        <div key={i} className="flex justify-center">
           {row.split('').map((char) => {
             const bgColor = store.exactGuesses.includes(char)
               ? 'bg-green-400'
@@ -16,6 +29,7 @@ export default observer(function Querty({ store }) {
               : 'bg-gray-200'
             return (
               <div
+                key={char}
                 className={`rounded-m m-px flex h-10 w-10 items-center justify-center uppercase text-black ${bgColor}`}
               >
                 {char}
