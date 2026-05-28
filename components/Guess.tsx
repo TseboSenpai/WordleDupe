802b0584631e6c21b3bd9e5770da721e5b14f34c
TseboSenpai <tsebom.dev@gmail.com>
2026-05-28 15:34:55 +0200

    Add small vertical spacing between guess rows in Guess component

--- components/Guess.tsx
+++ components/Guess.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function Guess({ isGuessed, guess, word }: Props) {
     return (
-        <div className="grid grid-cols-5 gap-2 md:gap-2">
+        <div className="grid grid-cols-5 gap-2 md:gap-2 mb-2">
             {new Array(5).fill(0).map((_, i) => {
                 const bgColor = !isGuessed
                     ? 'bg-black'
@@ -14,7 +14,7 @@ export default function Guess({ isGuessed, guess, word }: Props) {
                     ? 'bg-green-400'
                     : word.includes(guess[i])
                     ? 'bg-yellow-400'
-                    : 'bg-black';
+                    : 'bg-gray-400';
             
                 return (
                     <div key={i} className={`h-10 w-10 md:h-16 md:w-16 border border-gray-400 font-bold text-white uppercase flex items-center justify-center ${bgColor}`}>
